3ad8a4297391aa3454e70e2975f260ee6cadf5e6
kenaro <inbox@kenaro.com>
2019-09-09 13:58:08 +0200

    modify _ready_workflow and _patch.ps1 for better debugging from ISE

--- _ready_workflow/script.ps1
+++ _ready_workflow/script.ps1
@@ -2,7 +2,7 @@ param($wf, $eventData, $config)
 
 
 # This is the code you need to debug. It does not hurt the workflow during prdoction but can be removed of course. 
-if($wf-eq$null){import-module "c:\kenaflow\kenaflow.runtime.dll";Invoke-Kenaflow;exit}
+if($wf-eq$null){import-module "c:\kenaflow\kenaflow.runtime.dll";Invoke-Kenaflow -plugin "c:\source\plugin\bin\debug\kenaflow.demopluginv1.dll";exit}
 
 
 Write-KFLog (Get-PluginTestV1)
